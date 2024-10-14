ec5fffcd67662d1c7e3e29ea3c9cb7165f78b148
kevvie303 <117508458+kevvie303@users.noreply.github.com>
2024-10-14 23:16:54 +0200

    gooo

--- app.py
+++ app.py
@@ -808,15 +808,19 @@ def solve_task(task_name, room):
                 call_control_maglock_moonlight("herbalist-door-lock", "locked")
         elif task_name == "sigil-all":
             if game_status == {'status': 'playing'}:
+                publish.single("audio_control/mlv-central/play", "sigil-all-first.ogg", hostname=broker_ip)
+                time.sleep(12)
+                publish.single("audio_control/mlv-central/play", "sigil-all-second.ogg", hostname=broker_ip)
+                time.sleep(13)
                 send_dmx_command(0, 0, 0, 0, 255)
-                time.sleep(5) # time to play a sound that doesnt exist yet
-                send_dmx_command(0, 0, 0, 0, 0)
                 call_control_maglock_moonlight("lamp-post-1", "locked")
                 call_control_maglock_moonlight("lamp-post-2", "locked")
                 publish.single("audio_control/all_moonlight/full_stop", "stop", hostname=broker_ip)
                 publish.single("audio_control/mlv-central/play", "tense.ogg", hostname=broker_ip)
                 publish.single("audio_control/mlv-central/volume", "100 tense.ogg", hostname=broker_ip)
                 start_sequence()
+                time.sleep(5)
+                send_dmx_command(0, 0, 0, 0, 0)
         elif task_name == "paw-maze":
             if squeak_job == False:
                 scheduler.add_job(start_squeak, 'interval', seconds=30, id='squeakjob')
